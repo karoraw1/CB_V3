8375c9d304f058c92e056b82158754dfe572a233
karoraw1 <karoraw1@jhu.edu>
2019-05-20 16:46:58 -0400

    minor

--- otu_scripts/utility_scripts/callOTUs_skeleton.sh
+++ otu_scripts/utility_scripts/callOTUs_skeleton.sh
@@ -18,9 +18,9 @@ module load R/3.5.1
 SEQ_ID=@SID@
 BASE_OUT=/home-3/karoraw1@jhu.edu/work/sprehei1/Keith_Files/Processed_data_group
 SUFF1=_F_filt
-SUFF2=_R_filt 
+SUFF2=_R_filt
 SAMSPLIT=$SUFF1
 THREADS=24
-SCRIPTS_=~/scratch/ChesBayTransect/scripts
+SCRIPTS_=~/scratch/CB_V3/otu_scripts/utility_scripts
 
 Rscript $SCRIPTS_/fullDADApipe_PE.R $BASE_OUT $SEQ_ID $SUFF1 $SUFF2 $SAMSPLIT $THREADS
