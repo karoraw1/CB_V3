6dade305d5b66aa42094080a88b7587e609763ed
karoraw1 <karoraw1@jhu.edu>
2019-05-21 11:09:33 -0400

    finished homogeneous trimming

--- otu_scripts/utility_scripts/filter_skeleton.sh
+++ otu_scripts/utility_scripts/filter_skeleton.sh
@@ -19,13 +19,13 @@ module load R/3.5.1
 SCRIPTS_=~/scratch/CB_V3/otu_scripts/utility_scripts
 TSTAT=~/scratch/CB_V3/otu_data/TrimParams.csv
 TRIM_DATA=~/scratch/CB_V3/otu_data/trim_stats
-mkdir -p $TRIM_DATA
 SEQ_ID=@SID@
 BASE_OUT=~/work/sprehei1/Keith_Files/Processed_data_group
 DEMUX_DIR=$BASE_OUT/$SEQ_ID/FASTQ/Demux
 TRIM_DIR=$BASE_OUT/$SEQ_ID/FASTQ/Trim
 
-mkdir -p $TRIM_DIR
+mkdir -p $TRIM_DIR $TRIM_DATA/FASTQC_Summaries $TRIM_DATA/Trim_Plots
+
 Rscript $SCRIPTS_/FilterNTrim.R $SEQ_ID $DEMUX_DIR $TSTAT $TRIM_DIR
 
 source activate otu_caller
@@ -40,12 +40,11 @@ cat $TRIM_DIR/*_R_filt.fastq.sample > $BASE_OUT/$SEQ_ID/FASTQ/RandomSampleT.R2.f
 rm $TRIM_DIR/*_filt.fastq.sample
 
 mkdir $BASE_OUT/$SEQ_ID/FASTQ/pre-QC_report;
-fastqc -q -t 24 -o $BASE_OUT/$SEQ_ID/FASTQ/pre-QC_report -f fastq \
-$BASE_OUT/$SEQ_ID/FASTQ/RandomSampleT.R1.fastq \
-$BASE_OUT/$SEQ_ID/FASTQ/RandomSampleT.R2.fastq
-mv FASTQ/pre-QC_report/RandomSampleT.R1_fastqc.html $TRIM_DATA/$SEQ_ID.R1_fastqc.html
-mv FASTQ/pre-QC_report/RandomSampleT.R2_fastqc.html $TRIM_DATA/$SEQ_ID.R2_fastqc.html
+export PERL5LIB=~/scratch/miniconda3/envs/otu_caller/bin
+fastqc -q -t 24 -o $BASE_OUT/$SEQ_ID/FASTQ/pre-QC_report -f fastq $BASE_OUT/$SEQ_ID/FASTQ/RandomSampleT.R1.fastq
+mv $BASE_OUT/$SEQ_ID/FASTQ/pre-QC_report/RandomSampleT.R1_fastqc.html $TRIM_DATA/FASTQC_Summaries/${SEQ_ID}_fastqc_report.html
 rm -rf FASTQ/pre-QC_report
 
 module load R/3.5.1
 Rscript $SCRIPTS_/quality_plot.R $BASE_OUT/$SEQ_ID FASTQ RandomSampleT ${SEQ_ID}_Trim
+mv $BASE_OUT/$SEQ_ID/FASTQ/${SEQ_ID}_Trim_R1_and_R2_quals.png $TRIM_DATA/Trim_Plots
